446426207ec4989be07d635738c7fbfbaf45cfd2
shaygan <info@facechat7.xzn.ir>
2016-05-29 20:35:45 +0430

    dd

--- save/get.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("_class.php");
-$game=new game_class();
-
-if (isset($_GET['name']) && isset($_GET['score'])){$game->insert_score();}
-
-if (isset($_GET['get_score'])) {$game->get_scores();}
-?>
