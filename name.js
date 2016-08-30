96d5572f1600c0a5aca3dba8c0480a0fd0efcc51
shaygan <shayeglin@users.noreply.github.com>
2016-08-30 13:29:21 +0430

    ابدیت جدید! دیگه نمیشه ردش کرد!

--- name.js
+++ name.js
@@ -1,1 +1,8 @@
-var name = prompt("سلام،اسم شریفتون چیه؟!");if (name == "") {alert("جواب دادن ضروریه!"); name = prompt("اسمت چیه؟!")}
+//var name = prompt("سلام،اسم شریفتون چیه؟!");if (name == "") {alert("جواب دادن ضروریه!"); name = prompt("اسمت چیه؟!")}
+var username = ""
+while (username == "")
+{
+username = prompt('لطفا نام خود را وارد نمایید')
+
+}
+alert(' خوش آمدید ' + username)
