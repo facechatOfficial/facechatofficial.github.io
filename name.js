07d847e9631039e9820d5d9979679f1aee818875
shaygan shokrollahi <shayeglin@users.noreply.github.com>
2016-10-21 20:51:44 +0330

    Update name.js

--- name.js
+++ name.js
@@ -1,6 +1,6 @@
 //var name = prompt("سلام،اسم شریفتون چیه؟!");if (name == "") {alert("جواب دادن ضروریه!"); name = prompt("اسمت چیه؟!")}
 var username = ""
-while (username == "")
+while (username == "" && username == "null" && username == "undefined")
 {
 username = prompt('لطفا نام خود را وارد نمایید')
 
